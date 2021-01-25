f374a288fe1cfa6ffd446b0f557d39da7631ac5d
br1esc <77226337+br1esc@users.noreply.github.com>
2021-01-25 17:27:25 +0100

    Update index.js

--- Ejercicio1/index.js
+++ Ejercicio1/index.js
@@ -10,3 +10,15 @@ const al = 23*13;
 const ab = 16*23;
 const Area = al+ 2*ab;
 console.log( 'Area', Area + ' cm2');
+
+
+//Generalización:
+const base = 23;
+const alto = 13;
+const ancho = 16;
+ 
+const volumenfinal = base * alto* ancho;
+console.log('volumenfinal', volumenfinal+ 'cm3')
+
+const areafinal = base * alto + 2 * base * ancho
+console.log ('areafinal', areafinal + ' cm2')
