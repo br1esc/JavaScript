3d14e84efdca11ab5bccf6b66abc48d1e9670465
br1esc <77226337+br1esc@users.noreply.github.com>
2021-01-25 18:37:36 +0100

    Ejercicio 4 JavaScript
    If...

---
+++ Ejercicio 4/index.js
@@ -0,0 +1,16 @@
+'use strict';
+var numero1 = 5;
+var numero2 = 8;
+
+if(numero1 < numero2) {
+    alert('número1 no es mayor que número2');
+}
+if(numero1 >= 0) {
+    alert('número1 es positivo');
+}
+if(numero1 < 0, numero1 != 0) {
+    alert('número1 es negativo o distinto de 0');
+}
+if(++numero1 < numero2) {
+    alert('Incremetar en 1 unidad el valor de número1 no lo hace mayor o igual que numero2');
+}
