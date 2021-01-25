e23a835a50b294c10e31b95d6f5cc923a0e36bf9
br1esc <77226337+br1esc@users.noreply.github.com>
2021-01-25 17:21:54 +0100

    Ejercicio1 JavaScript
    Ejercicio1 JavaScript

---
+++ Ejercicio1/index.js
@@ -0,0 +1,12 @@
+// base 16*23
+//altura 13
+//Volumen= base * altura
+const volumen =16*23*13
+console.log('volumen', volumen +' cm3');
+
+//Area= al+ 2ab
+
+const al = 23*13;
+const ab = 16*23;
+const Area = al+ 2*ab;
+console.log( 'Area', Area + ' cm2');
