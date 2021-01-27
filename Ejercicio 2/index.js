341920031858d6561c2f7ba23cfafaf3b19415ba
br1esc <77226337+br1esc@users.noreply.github.com>
2021-01-27 17:04:53 +0100

    Create index.js

---
+++ Ejercicio 2/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const celta ={ 
+    day: 24,
+    month: 'Julio'
+};
+console.log( 'Mi cumpleaños es el '+ celta.day+ ' de '+ celta.month)
+
+const año = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+console.log( año[7] )
