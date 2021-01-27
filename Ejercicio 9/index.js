5782011ac88a49c688e887671c80066dfe8d6894
br1esc <77226337+br1esc@users.noreply.github.com>
2021-01-27 15:59:31 +0100

    Ejercicio 9
    Transforma un array de palabras en otro donde las palabras estén escritas en mayúsculas y donde el orden de los temas sea el inverso.

---
+++ Ejercicio 9/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const topics = ['JavaScript', 'Variables', 'funciones', 'condicionales', 'bucles'];
+const scipot = topics.reverse ( );
+const SCIPOT = scipot.map(eachTopic =>eachTopic.toUpperCase());
+
+
+console.log(SCIPOT);
